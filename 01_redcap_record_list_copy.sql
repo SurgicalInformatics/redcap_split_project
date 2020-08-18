fed96bd6e80a3a61dba67cf5f9a3fe8b43e7d253
Tim Shaw <tim.shaw@medsci.ox.ac.uk>
2020-08-18 08:54:18 +0100

    Update 01_redcap_record_list_copy.sql

--- 01_redcap_record_list_copy.sql
+++ 01_redcap_record_list_copy.sql
@@ -8,6 +8,13 @@
 -- After running this script, empty records (grey buttons)
 -- should show up in the interface
 -- Riinu Pius 14-Aug 2020
+
+-- mofified to use variables
+-- Tim Shaw 18-Aug-2020 
+
+SET @source_project = 64, @target_project = 68;
+SET @DAG_ids = '5319, 5457'
+
 insert into redcap_record_list
 select target_dag.project_id,
        source_record.arm,
@@ -17,6 +24,6 @@ select target_dag.project_id,
 	from redcap_record_list source_record
     inner join redcap_data_access_groups source_dag on source_dag.group_id = source_record.dag_id
     inner join redcap_data_access_groups target_dag on target_dag.group_name = source_dag.group_name
-        where source_dag.project_id = 64
-          and target_dag.project_id = 68
-          and source_record.dag_id in (5319, 5457)
+        where source_dag.project_id = @source_project
+          and target_dag.project_id = @target_project
+          and source_record.dag_id in @DAG_ids
