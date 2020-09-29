331618c30d4216f134c8e5f7015a7e783aa74761
Riinu <riinuots@gmail.com>
2020-09-29 15:09:19 +0100

    code to copy admin staff

---
+++ admin_staff/01_test2_move_admin_staff.sql
@@ -0,0 +1,2 @@
+INSERT INTO redcap_user_rights (project_id,username,role_id) VALUES (50, 'alison_gummery', 266);
+INSERT INTO redcap_user_rights (project_id,username,role_id) VALUES (51, 'alison_gummery', 276);
