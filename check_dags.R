e9f9e71396c5cc94b30f5d080714d7c2e2df7ff1
Riinu <riinuots@gmail.com>
2020-09-30 18:16:08 +0100

    n dags to be removed from each project

--- check_dags.R
+++ check_dags.R
@@ -1,14 +1,8 @@
 library(tidyverse)
 
-regions_lookup_orig = read_csv("dags_regions.csv")
+regions_lookup = read_csv("dags_regions.csv")
 
 dags_today = read_delim("check_dags_29-Sep.txt", delim = "\t")
-
-add_missing = tibble(dag_label = "Northamptonshire Healthcare Foundation Trust",
-                     nhs_region = "East Midlands",
-                     group_id = 2475)
-
-regions_lookup = bind_rows(regions_lookup_orig, add_missing)
 
 not_in_lookup = dags_today %>% 
   filter(! group_name %in% regions_lookup$dag_label)
@@ -25,3 +19,14 @@ dags_today %>%
   left_join(regions_lookup, by = c("group_name" = "dag_label")) %>% 
   filter(group_id.x != group_id.y)
 
+
+final_dags = regions_lookup %>% 
+  filter(dag_label %in% dags_today$group_name |
+           dag_label == "Queen Victoria Hospital")
+
+final_dags %>% 
+  count(nhs_region, name = "n_real_dags") %>% 
+  mutate(total = sum(n_real_dags),
+         to_be_removed = total-n_real_dags) %>% 
+  drop_na() %>% 
+  write_csv("dags_remove_count.csv")
